e505eaa81414f7bc0843afdfe4430b4059948c99
Felicien FOTIO MANFO <felicienfotiomanfo@Feliciens-MacBook-Pro.local>
2019-10-05 17:31:25 -0700

    Fist round defining types/aggregates -- Need to dicuss with Didier

--- .stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/Inventory-exe/autogen/cabal_macros.h
+++ .stack-work/dist/x86_64-osx/Cabal-2.4.0.1/build/Inventory-exe/autogen/cabal_macros.h
@@ -11,16 +11,16 @@
   (major1) == 0 && (major2) == 1 && (minor) <= 0)
 #endif /* MIN_VERSION_Inventory */
 
-/* package aeson-1.4.5.0 */
-#ifndef VERSION_aeson
-#define VERSION_aeson "1.4.5.0"
-#endif /* VERSION_aeson */
-#ifndef MIN_VERSION_aeson
-#define MIN_VERSION_aeson(major1,major2,minor) (\
-  (major1) <  1 || \
-  (major1) == 1 && (major2) <  4 || \
-  (major1) == 1 && (major2) == 4 && (minor) <= 5)
-#endif /* MIN_VERSION_aeson */
+/* package Inventory-0.1.0.0 */
+#ifndef VERSION_Inventory
+#define VERSION_Inventory "0.1.0.0"
+#endif /* VERSION_Inventory */
+#ifndef MIN_VERSION_Inventory
+#define MIN_VERSION_Inventory(major1,major2,minor) (\
+  (major1) <  0 || \
+  (major1) == 0 && (major2) <  1 || \
+  (major1) == 0 && (major2) == 1 && (minor) <= 0)
+#endif /* MIN_VERSION_Inventory */
 
 /* package base-4.12.0.0 */
 #ifndef VERSION_base
@@ -32,50 +32,6 @@
   (major1) == 4 && (major2) <  12 || \
   (major1) == 4 && (major2) == 12 && (minor) <= 0)
 #endif /* MIN_VERSION_base */
-
-/* package containers-0.6.0.1 */
-#ifndef VERSION_containers
-#define VERSION_containers "0.6.0.1"
-#endif /* VERSION_containers */
-#ifndef MIN_VERSION_containers
-#define MIN_VERSION_containers(major1,major2,minor) (\
-  (major1) <  0 || \
-  (major1) == 0 && (major2) <  6 || \
-  (major1) == 0 && (major2) == 6 && (minor) <= 0)
-#endif /* MIN_VERSION_containers */
-
-/* package servant-server-0.16.2 */
-#ifndef VERSION_servant_server
-#define VERSION_servant_server "0.16.2"
-#endif /* VERSION_servant_server */
-#ifndef MIN_VERSION_servant_server
-#define MIN_VERSION_servant_server(major1,major2,minor) (\
-  (major1) <  0 || \
-  (major1) == 0 && (major2) <  16 || \
-  (major1) == 0 && (major2) == 16 && (minor) <= 2)
-#endif /* MIN_VERSION_servant_server */
-
-/* package wai-3.2.2.1 */
-#ifndef VERSION_wai
-#define VERSION_wai "3.2.2.1"
-#endif /* VERSION_wai */
-#ifndef MIN_VERSION_wai
-#define MIN_VERSION_wai(major1,major2,minor) (\
-  (major1) <  3 || \
-  (major1) == 3 && (major2) <  2 || \
-  (major1) == 3 && (major2) == 2 && (minor) <= 2)
-#endif /* MIN_VERSION_wai */
-
-/* package warp-3.2.28 */
-#ifndef VERSION_warp
-#define VERSION_warp "3.2.28"
-#endif /* VERSION_warp */
-#ifndef MIN_VERSION_warp
-#define MIN_VERSION_warp(major1,major2,minor) (\
-  (major1) <  3 || \
-  (major1) == 3 && (major2) <  2 || \
-  (major1) == 3 && (major2) == 2 && (minor) <= 28)
-#endif /* MIN_VERSION_warp */
 
 /* tool alex-3.2.4 */
 #ifndef TOOL_VERSION_alex
@@ -187,11 +143,8 @@
   (major1) == 8 && (major2) == 6 && (minor) <= 5)
 #endif /* MIN_TOOL_VERSION_runghc */
 
-#ifndef CURRENT_PACKAGE_KEY
-#define CURRENT_PACKAGE_KEY "Inventory-0.1.0.0-8XJoBRaGxMyEj7jCdlHfF7"
-#endif /* CURRENT_PACKAGE_KEY */
 #ifndef CURRENT_COMPONENT_ID
-#define CURRENT_COMPONENT_ID "Inventory-0.1.0.0-8XJoBRaGxMyEj7jCdlHfF7"
+#define CURRENT_COMPONENT_ID "Inventory-0.1.0.0-CAznhXVVdst5IRV4Lf63dw-Inventory-exe"
 #endif /* CURRENT_COMPONENT_ID */
 #ifndef CURRENT_PACKAGE_VERSION
 #define CURRENT_PACKAGE_VERSION "0.1.0.0"
--- src/Types.hs
+++ src/Types.hs
@@ -6,71 +6,11 @@ import Data.Set
 
 
 
-regLostItem1 :: RegisteredLostItem
-regLostItem1 = RegisteredLostItem {
-      liIdentifier = Identifier "regLostItem1"
-    , liName = Name "Car keys"
-    , liCategoryId = CategoryIdentifier (Identifier "category1")  PersonalItems
-    , lostLocation = Location { 
-           region = Adamaoua
-        ,  department = Department (Djerem "Djerem") Adamaoua
-        ,  subdivision = SubDivision {
-               subDivisionCode = Gouandal "Gouandal" 
-            ,  departementCode = Department (Djerem "Djerem") Adamaoua
-            ,  regionCode = Adamaoua
-           }
-        ,  city = City "Ngaoundere"
-        ,  neighborhood = Neighborhood "Chefferie"
-        ,  address = Address "Rond point 3 morts"
-        }
-    , liDescription = 
-        Description "Mes cles sont attachees a un ruban rouge et la marque de ma voiture c'est toyota"
-    , liLostDate = "21/01/1985"
-    , liAttributes = [Color "red", Brand "Toyota", Size  "smal"] 
-}
-
-
-
-regLostItem2 :: RegisteredLostItem
-regLostItem2 = RegisteredLostItem {
-      liIdentifier = Identifier "regLostItem2"
-    , liName = Name "National Identity Card"
-    , liCategoryId = CategoryIdentifier (Identifier "category2") PersonalItems
-    , lostLocation = Location { 
-           region = Adamaoua
-        ,  department = Department (Djerem "Djerem") Adamaoua
-        ,  subdivision = SubDivision {
-               subDivisionCode = Gouandal "Gouandal" 
-            ,  departementCode = Department (Djerem "Djerem") Adamaoua
-            ,  regionCode = Adamaoua
-           }
-        ,  city = City "Ngaoundere"
-        ,  neighborhood = Neighborhood "Chefferie"
-        ,  address = Address "Rond point 3 morts"
-        }
-    , liDescription = 
-        Description "Mes cles sont attaches a un ruban rouge et la marque de ma voiture c'est toyota"
-    , liLostDate = "22/01/1985"
-    , liAttributes = [] 
-}
-
-
-
-
-
-regLostItems = M.fromList [
-    (liIdentifier regLostItem1, regLostItem1), 
-    (liIdentifier regLostItem2, regLostItem2)
-    ]
-
-
 data Item = 
       Lost RegisteredLostItem
     | Found DeclaredFoundItem
+    | Matched LostAndFoundItem
     | Claimed ClaimedFoundItem
-
-
-
 
 
 data RegisteredLostItem = RegisteredLostItem {
@@ -81,6 +21,8 @@ data RegisteredLostItem = RegisteredLostItem {
     ,   liDescription :: Description
     ,   liLostDate :: String
     ,   liAttributes :: [Attribute]
+    ,   liOwner :: Person
+
     } deriving (Eq, Ord, Show)
 
 
@@ -92,18 +34,24 @@ data DeclaredFoundItem = DeclaredFoundItem {
     ,   fiDescription :: Description
     ,   fiFoundDate :: String
     ,   fiAttributes :: [Attribute]
+    ,   fiDeclarant :: Person
     } deriving (Eq, Ord, Show)
+
+
+data LostAndFoundItem = LostAndFoundItem {
+        lfiIdentifier :: Identifier
+    ,   registeredLostItem :: RegisteredLostItem
+    ,   declaredFoundItems :: [DeclaredFoundItem]
+    } deriving (Eq, Ord, Show)  
+
 
 data ClaimedFoundItem = ClaimedFoundItem {
         clIdentifier :: Identifier
-    ,   clName :: Name
-    ,   clCategoryId :: CategoryIdentifier
-    ,   clfoundLocation :: Location
-    ,   clDescription :: Description
-    ,   clFoundDate :: String
-    ,   clAttributes :: [Attribute]
-
+    ,   clRegisteredLostItem :: RegisteredLostItem
+    ,   clDeclaredFoundItem :: DeclaredFoundItem
+    ,   matchedId :: Maybe Identifier
     ,   claimedDate :: String
+    ,   clClaimer :: Person
     } deriving (Eq, Ord, Show)   
 
 
@@ -125,10 +73,12 @@ data Category = Category {
 data CategoryIdentifier = 
     CategoryIdentifier Identifier CategoryType deriving (Eq, Ord, Show)
 
+
 data ParentalStatus =
       Parent
     | Sub
     deriving (Eq, Ord, Show)
+
 
 data CategoryType = 
       Humans 
@@ -137,76 +87,611 @@ data CategoryType =
     | PersonalItems
     deriving (Eq, Ord, Show)
 
+
 data CategoryDetails = CategoryDetails {
         categoryCode :: CategoryCode
     ,   categoryName :: CategoryName
     ,   categoryDescription :: CategoryDescription
 } deriving (Eq, Ord, Show)
 
+
 newtype CategoryCode =
     CategoryCode String deriving (Eq, Ord, Show)
 
+
 newtype CategoryName =
     CategoryName String deriving (Eq, Ord, Show)
+
 
 newtype CategoryDescription =
     CategoryDescription String deriving (Eq, Ord, Show)
 
 
-
 data Location = Location {
         region :: Region
-    ,   department :: Department
+    ,   division :: Division
     ,   subdivision :: SubDivision
     ,   city :: City
+    ,   village :: Village
     ,   neighborhood :: Neighborhood
-    ,   address :: Address
+    ,   loAddress :: Address
 } deriving (Eq, Ord, Show)
 
 
 data Region 
-    = North
-    | South
-    | West
-    | East
-    | Center
-    | NorthWest
-    | SouthWest
-    | ExtremeNorth
-    | Adamaoua
+    = Adamaoua
+    | Centre
+    | Est
+    | FarNorth
     | Littoral
+    | North
+    | NorthWest
+    | South
+    | SouthWest
+    | West
     deriving (Eq, Ord, Show)
 
 
-data Department = 
-    Department DepartmentCode Region deriving (Eq, Ord, Show)
-
-
-data DepartmentCode 
+data Division
+--    Adamaoua 
     = Djerem String
     | FaroEtDeo String
     | MayoBanyo String
     | Mbere String
     | Vina String
+
+--    Centre
+    | HauteSanaga String
+    | Lekie String
+    | MbamEtInoubou String
+    | MbamEtKim String
+    | MefouEtAfamba String
+    | MefouEtAkono String
+    | Mfoundi String
+    | NyongEtKelle String
+    | NyongEtMfoumou String
+    | NyongEtSoo String
+
+--    Est
+    | BoumbaEtNgoko String
+    | HautNyong String
+    | Kadey String
+    | LomEtDjerem String
+
+--    Far North
+    | Diamare String
+    | LogoneEtChari String
+    | MayoDanay String
+    | MayoKani String
+    | MayoSava String
+    | MayoTsanaga String
+
+--    Littoral
+    | Moungo String
+    | Nkam String
+    | SanagaMaritime String
+    | Wouri String
+
+--    North
+
+    | Benoue String
+    | Faro String
+    | MayoLouti String
+    | MayoRey String
+
+--    NorthWest
+    | Boyo String
+    | Bui String
+    | DongaMantung String
+    | Menchum String
+    | Mezam String
+    | Momo String
+    | Ngoketunjia String
+
+--    South
+    | DjaEtLobo String
+    | Mvila String
+    | Ocean String
+    | ValleeDuNtem String
+
+--    SouthWest
+    | Fako String
+    | KoupeManengouba String
+    | Lebialem String
+    | Manyu String
+    | Meme String
+    | Ndian String
+
+--    West
+    | Bamboutos String
+    | HautNkam String
+    | HautsPlateaux String
+    | KoungKhi String
+    | Menoua String
+    | Mifi String
+    | Nde String
+    | Noun String
     deriving (Eq, Ord, Show)
 
     
 
-data SubDivision = SubDivision {
-        subDivisionCode :: SubDivisionCode 
-    ,   departementCode :: Department
-    ,   regionCode :: Region
-} deriving (Eq, Ord, Show)
 
+data SubDivision
+-- ADAMAOUA
 
-data SubDivisionCode 
+--  Djerem
     = Gouandal String
-    | Tibatie String
+    | Tibati String
+--  Faro et Deo 
+    | GalimTignere
+    | MayoBaleo 
+    | Tignere
+--  Mayo Banyo
+    | Bankim
+    | Banyo
+    | MayoDarl
+-- Mbere
+    | Dir
+    | Djohong
+    | Meiganga
+    | Ngaoui
+-- Vina 
+    | Belel
+    | Mbe
+    | Nganha
+    | NgaoundereIer
+    | NgaoundereIIe
+    | NgaoundereIIIe
+    | Nyambaka
+    | Martap
+
+
+-- CENTRE 
+
+
+-- Haute Sanaga
+    | Bibey
+    | LembeYezoum
+    | Mbandjock
+    | Minta
+-- Lekie
+    | Batchenga
+    | Ebebda
+    | EligMfomo
+    | Evodoula
+    | Lobo
+    | Monatélé
+    | Obala
+    | Okola
+    | Saa
+-- Mbam Et Inoubou
+    | Bafia
+    | Bokito
+    | Deuk
+    | Kiiki
+    | KonYambetta
+    | Makenene
+    | Ndikiniméki
+    | Nitoukou
+    | Ombessa
+-- Mbam et Kim
+    | Mbangassina
+    | NgambeTikar
+    | Ngoro
+    | Ntui
+    | Yoko
+-- MefouEtAfamba 
+    | Afanloum
+    | AssambaOrOlanguina
+    | Awae
+    | Edzendouan
+    | Esse
+    | Mfou
+    | Nkolafamba
+    | Soa
+-- MefouEtAkono 
+    | Akono
+    | Bikok
+    | Mbankomo
+    | Ngoumou
+-- Mfoundi 
+    | YaoundeIOrNlongkakOrEtoudi
+    | YaoundeIIOrTsinga
+    | YaoundeIIIOrEfoulan
+    | YaoundeOrIVKondengui
+    | YaoundeVOrEssos
+    | YaoundeVIOrBiyemAssi
+    | YaoundeVIIOrNkolbisson
+-- NyongEtKelle 
+    | Biyouha
+    | Bondjock
+    | BotMakak
+    | Dibang
+    | Eseka
+    | Makak
+    | Matomb
+    | Messondo
+    | NgogMapubi
+    | Nguibassal
+-- NyongEtMfoumou 
+
+    | Akonolinga
+    | Ayos
+    | Endom
+    | Mengang
+    | NyakokomboOrKobdombo
+-- NyongEtSoo 
+
+    | Akoeman
+    | Dzeng
+    | Mbalmayo
+    | Mengueme
+    | Ngomedzap
+    | Nkolmetet
+
+-- EST
+
+-- BoumbaEtNgoko 
+    | GariGombo
+    | Moloundou
+    | Salapoumbe
+    | Yokadouma
+-- HautNyong
+    | AbongMbang
+    | BebendOrAtok
+    | Dimako
+    | Doumaintang
+    | Doume
+    | Lomie
+    | MboanzOrAngossas
+    | Mboma
+    | Messamena
+    | Messok
+    | Mindourou
+    | Ngoyla
+    | Nguelemendouka
+    | Somalomo
+-- Kadey 
+    | Batouri
+    | Kentzou
+    | Kette
+    | Mbang
+    | Ndelele
+    | Nguelebok
+    | Ouli
+-- LomEtDjerem 
+    | Belabo
+    | BertouaIer
+    | BertouaIIe
+    | BétareOya
+    | Diang
+    | GarouaBoulaï
+    | Mandjou
+    | Ngoura
+
+-- FAR NORTH
+
+-- Diamare 
+    | Bogo
+    | Dargala
+    | Gazawa
+    | MarouaIer
+    | MarouaIIe
+    | MarouaIIIe
+    | Meri
+    | Ndoukoula
+    | Pette
+-- LogoneEtChari 
+    | Blangoua
+    | Darak
+    | Fotokol
+    | Goulfey
+    | HileAlifa
+    | Kousseri
+    | LogoneBirni
+    | Makary
+    | Waza
+    | Zina
+-- MayoDanay 
+    | Datcheka
+    | Gobo
+    | Gueme
+    | Guere
+    | KaiKai
+    | Kalfou
+    | KarHay
+    | Maga
+    | TchatiBali
+    | Wina
+    | Yagoua
+
+-- MayoKani 
+    | Dziguilao -- ??? Commune ou arrondiseement ???
+    | Taibong
+    | Guidiguis
+    | Kaele
+    | Mindif
+    | Moulvoudaye
+    | Moutourwa
+    | Touloum
+-- MayoSava
+    | Kolofata
+    | Mora
+    | Tokombere
+
+-- MayoTsanaga 
+    | Bourrha
+    | Hina
+    | Koza
+    | Mogode
+    | Mokolo
+    | Mozogo
+    | SouledeRoua
+
+-- LITTORAL
+
+-- Moungo 
+    | BareBakem
+    | Bonalea
+    | Dibombari
+    | Loum
+    | Manjo
+    | Mbanga
+    | Melong
+    | Mombo
+    | NjombePenjaOrPenja
+    | NkongsambaIer
+    | NkongsambaIIe
+    | NkongsambaIIIe
+    | NlonakoOrEbone
+-- Nkam 
+    | Nkondjock
+    | NordMakombeOrNdobian
+    | Yabassi
+    | Yingui
+-- SanagaMaritime 
+    | Dibamba
+    | Dizangue
+    | EdeaIer
+    | EdeaIIe
+    | MassockSongloulou
+    | Mouanko
+    | Ndom
+    | Ngambe
+    | Ngwei
+    | Nyanon
+    | Pouma
+-- Wouri 
+    | DoualaIer
+    | DoualaIIe
+    | DoualaIIIe
+    | DoualaIVe
+    | DoualaVe
+    | DoualaVIe
+    | Manoka
+
+
+-- NORTH
+
+
+-- Benoue 
+    | Bibemi
+    | Dembo
+    | GarouaIer
+    | GarouaIIe
+    | GarouaIIIe
+    | Lagdo
+    | MayoHourna
+    | Pitoa
+    | Tcheboa
+    | Demsa
+    | Bascheo
+-- Faro 
+    | Beka
+    | Poli
+-- MayoLouti 
+    | Figuil
+    | Guider
+    | MayoOulo
+-- MayoRey 
+    | Madingring
+    | ReyBouba
+    | Tchollire
+    | Touboro
+ 
+
+-- NORTH WEST
+
+
+-- Boyo
+    | Belo
+    | Fonfuka
+    | Fundong
+    | Njinikom
+-- Bui 
+    | Jakiri
+    | Kumbo
+    | MbvenOrMbiame
+    | Nkum
+    | NoniOrNkor
+    | OkuOrElakOku
+-- DongaMantung 
+    | Ako
+    | Misaje
+    | Ndu
+    | Nkambe
+    | Nwa
+-- Menchum 
+    | Benakuma
+    | FuruAwa
+    | Wum
+    | Zhoa
+-- Mezam 
+    | Bafut
+    | Bali
+    | BamendaIer
+    | BamendaIIe
+    | BamendaIIIe
+    | Santa
+    | Tubah
+-- Momo 
+    | Andek
+    | Batibo
+    | Mbengwi
+    | Njikwa
+    | WidikumBoffe
+-- Ngoketunjia
+
+    | Babessi
+    | Balikumbat
+    | Ndop
+
+
+
+-- SOUTH 
+
+
+-- DjaEtLobo 
+    | Bengbis
+    | Djoum
+    | Meyomessala
+    | Meyomessi
+    | Mintom
+    | Oveng
+    | Sangmelima
+    | Zoetele
+-- Mvila
+    | BiwongBane
+    | BiwongBulu
+    | EbolowaIer
+    | EbolowaIIe
+    | Efoulan
+    | Mengong
+    | Mvangan
+    | Ngoulemakong
+-- Ocean
+    | AkomII
+    | Bipindi
+    | Campo
+    | KribiIer
+    | KribiIIe
+    | Lokoundje
+    | Lolodorf
+    | Mvengue
+    | Niete
+-- ValleeDuNtem
+    | Ambam
+    | KyeOssi
+    | Maan
+    | Olamze
+
+
+
+-- SOUTH WEST
+
+
+-- Fako
+    | Buea
+    | LimbeIer
+    | LimbeIIe
+    | LimbeIIIe
+    | Muyuka
+    | Tiko
+    | WestCoast
+-- KoupeManengouba
+    | Bangem
+    | Nguti
+    | Tombel
+-- Lebialem 
+    | Alou
+    | Fontem
+    | Wabane
+-- Manyu
+    | Akwaya
+    | Eyumodjock
+    | Mamfe
+    | TintoOrUpperBayang
+-- Meme 
+    | Konye
+    | KumbaIer
+    | KumbaIIe
+    | KumbaIIIe
+    | Mbonge
+-- Ndian
+    | Bamusso
+    | EkondoTiti
+    | Idabato
+    | Isanguele
+    | KomboAbedimo
+    | KomboItindi
+    | Mundemba
+
+
+
+
+-- WEST
+
+
+
+-- Bamboutos
+    | Babadjou
+    | Batcham
+    | Galim
+    | Mbouda
+-- Haut Nkam 
+    | Bafang
+    | Bakou
+    | Bana
+    | Bandja
+    | Banka
+    | Kekem
+-- Hauts Plateaux
+    | Baham
+    | Bamendjou
+    | Bangou
+    | Batie
+    | Bangam
+    | Badenkop
+-- KoungKhi
+    | Bayangam
+    | Demdeng
+    | Poumougne
+-- Menoua 
+    | Dschang
+    | Fokoue
+    | FongoTongo
+    | NkongZem
+    | PenkaMichel
+    | Santchou
+-- Mifi
+    | BafoussamIer
+    | BafoussamIIe
+    | BafoussamIIIe
+-- Nde
+    | Bangangte
+    | Bassamba
+    | Bazou
+    | Tonga
+-- Noun 
+    | Bangourain
+    | Foumban
+    | Foumbot
+    | Kouoptamo
+    | Koutaba
+    | Magba
+    | Malentouen
+    | Massangam
+    | Njimom
     deriving (Eq, Ord, Show)
+
+
 
 
 newtype City = 
     City String deriving (Eq, Ord, Show)
+
+newtype Village = 
+    Village String deriving (Eq, Ord, Show)
 
 newtype Neighborhood = 
     Neighborhood String deriving (Eq, Ord, Show)
@@ -217,7 +702,6 @@ newtype Address =
 
 newtype Description = 
     Description String deriving (Eq, Ord, Show)
-
 
 
 data Attribute = 
@@ -232,8 +716,41 @@ data Attribute =
     deriving (Eq, Ord, Show)
 
 
+data Person = Person {
+      user :: Identifier
+    , contact :: ContactInformation
+    , name :: FullName
+} deriving (Eq, Ord, Show)
 
 
+data ContactInformation = ContactInformation {
+      email :: EmailAddress
+    , address :: PostalAddress
+    , primaryTel :: Telephone
+    , secondaryTel :: Telephone
+} deriving (Eq, Ord, Show)
+
+data FullName = FullName {
+      first :: FirstName
+    , middle :: Maybe Middle
+    , last :: LastName
+} deriving (Eq, Ord, Show)
+
+newtype FirstName = 
+    FirstName String deriving (Eq, Ord, Show)
+
+newtype Middle = 
+    Middle String deriving (Eq, Ord, Show)
+
+newtype LastName = 
+    LastName String deriving (Eq, Ord, Show)
+
+newtype EmailAddress = 
+    EmailAddress String deriving (Eq, Ord, Show)
+newtype PostalAddress = 
+    PostalAddress String deriving (Eq, Ord, Show)
+newtype Telephone = 
+    Telephone String deriving (Eq, Ord, Show)
 newtype AttributeCode = 
     AttributeCode String deriving (Eq, Ord, Show)
 newtype AttributeName = 
@@ -246,6 +763,66 @@ newtype AttributeUnit =
 
 
 
+
+
+
+
+---------- TESTING ---------
+{-- 
+regLostItem1 :: Item 
+regLostItem1 = Lost RegisteredLostItem {
+      liIdentifier = Identifier "regLostItem1"
+    , liName = Name "Car keys"
+    , liCategoryId = CategoryIdentifier (Identifier "category1")  PersonalItems
+    , lostLocation = Location { 
+           region = Adamaoua
+        ,  division = Djerem "Djerem"
+        ,  subdivision = Gouandal "Gouandal" 
+        ,  city = City "Ngaoundere"
+        ,  village = Village "Ngaoundere"
+        ,  neighborhood = Neighborhood "Chefferie"
+        ,  loAddress = Address "Rond point 3 morts"
+        }
+    , liDescription = 
+        Description "Mes cles sont attachees a un ruban rouge et la marque de ma voiture c'est toyota"
+    , liLostDate = "21/01/1985"
+    , liAttributes = [Color "red", Brand "Toyota", Size  "smal"] 
+}
+regLostItem2 :: Item
+regLostItem2 = Lost RegisteredLostItem {
+      liIdentifier = Identifier "regLostItem2"
+    , liName = Name "National Identity Card"
+    , liCategoryId = CategoryIdentifier (Identifier "category2") PersonalItems
+    , lostLocation = Location { 
+           region = Adamaoua
+        ,  division = Djerem "Djerem" 
+        ,  subdivision = Gouandal "Gouandal"
+        ,  city = City "Ngaoundere"
+        ,  village = Village "Ngaoundere"
+        ,  neighborhood = Neighborhood "Chefferie"
+        ,  loAddress = Address "Carrefour 3 bléssés"
+        }
+    , liDescription = 
+        Description "Ma carte nationale d'identité est encore l'acien format"
+    , liLostDate = "22/01/1985"
+    , liAttributes = [] 
+}
+
+getItemIdentifier :: Item -> Identifier
+getItemIdentifier elem =
+    case elem of
+        Lost item -> liIdentifier item
+        Found item -> fiIdentifier item
+        Matched item -> lfiIdentifier item
+        Claimed item -> clIdentifier item
+
+
+regLostItems :: M.Map Identifier Item
+regLostItems = M.fromList [
+    (getItemIdentifier regLostItem1, regLostItem1), 
+    (getItemIdentifier regLostItem2, regLostItem2)
+    ]
+--}
 
 
 
